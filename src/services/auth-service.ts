664a6e7250c6b988eb550210891b5bc80fa5d3a3
Alexandre Borges <alexandre.bsilva55@gmail.com>
2021-03-22 20:40:34 -0300

    :passport_control: updating auth service

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -4,37 +4,43 @@ import { JwtSignature } from "../models/auth/jwt-payload";
 import { LoginPayloadReq } from "../models/auth/login-payload";
 import { getRepository, Repository } from "typeorm";
 import { User } from "../entities";
-import { encrypt, compareTextWithHash } from "../helpers/crypto";
+import { compareTextWithHash } from "../helpers/crypto";
 dotenv.config();
 
+export const name = "alexandre";
 export class AuthService {
   private userRepository: Repository<User>;
   constructor() {
     this.userRepository = getRepository(User);
   }
-  public createJwt(JwtSignature: JwtSignature) {
+  public createJwt(
+    JwtSignature: JwtSignature,
+    refreshToken: boolean = false
+  ): string {
     return jwt.sign(JwtSignature, process.env.JWT_PRIVATE_KEY!, {
       algorithm: "HS256",
-      expiresIn: "1h",
+      //900000 = 15 min
+      expiresIn: refreshToken ? "7d" : "900000ms",
     });
   }
 
-  public async getUserByCredentials(
-    loginForm: LoginPayloadReq
-  ): Promise<User | undefined> {
-    const userByEmail: User[] = await this.userRepository.find({
+  public async getUserByCredentials(loginForm: LoginPayloadReq): Promise<User> {
+    const userByEmail = await this.userRepository.findOneOrFail({
       where: { email: loginForm.email },
       relations: ["role"],
     });
-    if (userByEmail.length > 0) {
+
+    if (userByEmail) {
       const isPasswordCorrect = compareTextWithHash(
         loginForm.password,
-        userByEmail[0].password
+        userByEmail.password
       );
+
       if (isPasswordCorrect) {
-        return userByEmail[0];
+        return userByEmail;
       }
     }
-    return undefined;
+
+    throw new Error("Wrong credentials");
   }
 }
